7d04d650d3af6bbd0cc12e980d3256d128e53e26
Ondřej Cervan <238519@vutbr.cz>
2024-04-17 09:45:32 +0200

    commit č. 2

--- sorting.py
+++ sorting.py
@@ -23,9 +23,39 @@ def read_data(file_name):
 
         return data
 
-print(read_data("numbers.csv"))
+# print(read_data("numbers.csv"))
+
+# my_list = [1, 2, 3, 4]
+# my_list[1], my_list[3] = my_list[3], my_list[1]
+# print(my_list)
+"""
+def selection_sort(list):
+    output = []
+    i = 0
+    for number in range(len(list)):
+        if number[i] < list[i]:
+            output.append(number)
+        else:
+            i += 1
+
+    return output
+"""
+# print(selection_sort(read_data("numbers.csv")))
+
+def selection_sort(list):
+    for i in range(len(list)):
+        min_idx = i
+        for num_idx in range(i + 1, len(list)):
+            if list[min_idx] > list[num_idx]:
+                min_idx = num_idx
+        list[i], list[min_idx] = list[min_idx], list[i]
+
+    print(list)
+
 
 def main():
+    numbers = read_data("numbers.csv")
+    sorted_series = selection_sort(numbers["series_1"])
     pass
 
 
